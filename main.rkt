007a0bd75d8a282267073428894ef926ec0905c5
matja307 <matja307@student.liu.se>
2013-05-05 16:12:21 +0200

    Efter mittavstämning

--- main.rkt
+++ main.rkt
@@ -35,24 +35,28 @@
     (display (cdr (ask *board* 'count-bricks)))(newline)
   
   (let* ((player (*turn* *player-list*))
-        (player-color (ask (*turn* *player-list*) 'color))
-        (move (get-next-move player)))
-   
-    (if (null? move)
-        (begin
-          (swich-turn)
-          (main-loop))
-        (begin
-          (when (and (not (eq? move 'aborted)) (ask *board* 'valid-move? (x move) (y move) player-color)) 
-            
-            (ask *board* 'put-brick! (x move) (y move) player-color)
-            (ask *board* 'flip! (x move) (y move) player-color)
-            
-            (if (= 0 player-color)
-                (set-piece-at! (x move) (y move) 'BLACK)
-                (set-piece-at! (x move) (y move) 'WHITE))
-            
-            (swich-turn))))
-  (main-loop)))
+         (player-color (ask player 'color)))
+    
+    (cond ((and (or (null? (ask *board* 'valid-moves-list 0)) (= (ask (car *player-list*) 'bricks) 0))
+                (or (null? (ask *board* 'valid-moves-list 1)) (= (ask (cdr *player-list*) 'bricks) 0)))
+           (display "Game Over!")(newline))
+          ((or (null? (ask *board* 'valid-moves-list player-color)) (= (ask player 'bricks) 0))
+             (swich-turn)
+             (main-loop))
+          (else
+           (begin
+             (let ((move (get-next-move player)))
+               (when (and (not (eq? move 'aborted)) (ask *board* 'valid-move? (x move) (y move) player-color)) 
+                 
+                 (ask player 'countdown-bricks)
+                 (ask *board* 'put-brick! (x move) (y move) player-color)
+                 (ask *board* 'flip! (x move) (y move) player-color)
+                 
+                 (if (= 0 player-color)
+                     (set-piece-at! (x move) (y move) 'BLACK)
+                     (set-piece-at! (x move) (y move) 'WHITE))
+                 
+                 (swich-turn))
+               (main-loop)))))))
 
 (thread main-loop)
